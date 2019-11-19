7deffb17091a6f522749fd708cece687073526b7
Kevin Mitchell <kevin475@hotmail.com>
2019-11-19 03:55:33 -0800

    Initial

--- app/server.ts
+++ app/server.ts
@@ -6,50 +6,44 @@ import bodyparser from 'body-parser';
 
 const app = express();
 const jsonParser = bodyparser.json();
-const urlEncodedParser = bodyparser.urlencoded({extended: false});
-
-/*
-import * as bodyparser from "body-parser";
-export const jsonParser = bodyparser.json();
-export const urlEncodedParser = bodyparser.urlencoded({extended: true});
-*/
+const urlEncodedParser = bodyparser.urlencoded({ extended: false });
 
 app.get('/brokers', (req, res, next) => {
     console.log('brokers');
     res.send('Brokers go here!');
-} );
+});
 
 app.get('/brokers/:id', (req, res, next) => {
     const brokerId = req.params.id;
     console.log(`Broker with id: ${brokerId}`);
     res.send(`Broker with id: "${brokerId}" goes here!`);
-} );
+});
 
 app.delete('/brokers/:id', (req, res, next) => {
     const brokerId = req.params.id;
     console.log(`Delete broker with id: ${brokerId}`);
     res.send(`Delete broker with id: "${brokerId}"!`);
-} );
+});
 
 app.post('/brokers', jsonParser, (req, res, next) => {
     console.log(`Add a new broker`);
     console.log(req.body);
-    res.json({"testx": "just testx"});
-} );
+    res.json({ "testx": "just testx" });
+});
 
 app.put('/brokers/:id', jsonParser, (req, res, next) => {
     const brokerId = req.params.id;
     console.log(`Update (replace) broker with id: ${brokerId}`);
     res.send(`Update (replace) broker with id: "${brokerId}"!`);
-} );
+});
 
 app.patch('/brokers/:id', jsonParser, (req, res, next) => {
     const brokerId = req.params.id;
     console.log(`Update (only included fields) broker with id: ${brokerId}`);
     res.send(`Update (only included fields) broker with id: "${brokerId}"!`);
-} );
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server started and listening on port: ${PORT}`);
-} );
+});
--- build/server.js
+++ build/server.js
@@ -4,46 +4,41 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 console.log('Hello, World!');
-var express_1 = __importDefault(require("express"));
-var body_parser_1 = __importDefault(require("body-parser"));
-var app = express_1.default();
-var jsonParser = body_parser_1.default.json();
-var urlEncodedParser = body_parser_1.default.urlencoded({ extended: false });
-/*
-import * as bodyparser from "body-parser";
-export const jsonParser = bodyparser.json();
-export const urlEncodedParser = bodyparser.urlencoded({extended: true});
-*/
-app.get('/brokers', function (req, res, next) {
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const app = express_1.default();
+const jsonParser = body_parser_1.default.json();
+const urlEncodedParser = body_parser_1.default.urlencoded({ extended: false });
+app.get('/brokers', (req, res, next) => {
     console.log('brokers');
     res.send('Brokers go here!');
 });
-app.get('/brokers/:id', function (req, res, next) {
-    var brokerId = req.params.id;
-    console.log("Broker with id: " + brokerId);
-    res.send("Broker with id: \"" + brokerId + "\" goes here!");
+app.get('/brokers/:id', (req, res, next) => {
+    const brokerId = req.params.id;
+    console.log(`Broker with id: ${brokerId}`);
+    res.send(`Broker with id: "${brokerId}" goes here!`);
 });
-app.delete('/brokers/:id', function (req, res, next) {
-    var brokerId = req.params.id;
-    console.log("Delete broker with id: " + brokerId);
-    res.send("Delete broker with id: \"" + brokerId + "\"!");
+app.delete('/brokers/:id', (req, res, next) => {
+    const brokerId = req.params.id;
+    console.log(`Delete broker with id: ${brokerId}`);
+    res.send(`Delete broker with id: "${brokerId}"!`);
 });
-app.post('/brokers', jsonParser, function (req, res, next) {
-    console.log("Add a new broker");
+app.post('/brokers', jsonParser, (req, res, next) => {
+    console.log(`Add a new broker`);
     console.log(req.body);
     res.json({ "testx": "just testx" });
 });
-app.put('/brokers/:id', jsonParser, function (req, res, next) {
-    var brokerId = req.params.id;
-    console.log("Update (replace) broker with id: " + brokerId);
-    res.send("Update (replace) broker with id: \"" + brokerId + "\"!");
+app.put('/brokers/:id', jsonParser, (req, res, next) => {
+    const brokerId = req.params.id;
+    console.log(`Update (replace) broker with id: ${brokerId}`);
+    res.send(`Update (replace) broker with id: "${brokerId}"!`);
 });
-app.patch('/brokers/:id', jsonParser, function (req, res, next) {
-    var brokerId = req.params.id;
-    console.log("Update (only included fields) broker with id: " + brokerId);
-    res.send("Update (only included fields) broker with id: \"" + brokerId + "\"!");
+app.patch('/brokers/:id', jsonParser, (req, res, next) => {
+    const brokerId = req.params.id;
+    console.log(`Update (only included fields) broker with id: ${brokerId}`);
+    res.send(`Update (only included fields) broker with id: "${brokerId}"!`);
 });
-var PORT = process.env.PORT || 5000;
-app.listen(PORT, function () {
-    console.log("Server started and listening on port: " + PORT);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server started and listening on port: ${PORT}`);
 });
